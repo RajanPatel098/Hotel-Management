77a7fc4417944f87b64549e504c3c87e22362d18
RajanPatel098 <106442470+RajanPatel098@users.noreply.github.com>
2024-01-23 14:25:32 -0600

    create react

---
+++ hotel-management/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ReactDOM } from 'react';
+import { BrowserRouter } from "react-router-dom"
+
+function App() {
+  return (
+    <div>
+      <p> hello world </p>
+    </div>
+  );
+}
+
+export default App;
